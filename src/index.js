48f8b5450e2059f3d10abdad5b5c969b7acbb3d4
yuji <53387749+pierrejaune@users.noreply.github.com>
2020-08-14 20:32:57 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'; // storeを作成する為の関数
+import { Provider } from 'react-redux';// 作成したstoreを全コンポーネントに渡す為の機能をもつコンポーネント
+
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
