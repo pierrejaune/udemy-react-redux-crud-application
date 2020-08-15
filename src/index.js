970212d06e14a0fe7de6de5c00e7f3f4aa5ae645
yuji <53387749+pierrejaune@users.noreply.github.com>
2020-08-15 12:26:05 +0900

    implement events index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; // storeを作成する為の関数
+import { createStore, applyMiddleware } from 'redux'; // createStoreはstoreを作成する為の関数。redux-thunkはミドルウェアなのでapplyMiddlewareで使用可能にする
 import { Provider } from 'react-redux';// 作成したstoreを全コンポーネントに渡す為の機能をもつコンポーネント
+import thunk from 'redux-thunk';
 
 import './index.css';
 import reducer from './reducers';
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={ store }>
-    <App />
+    <EventsIndex />
   </Provider>,
   document.getElementById('root')
 );
