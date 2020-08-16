49f18a531a12e6b97afbc98fd7be882e7dda3214
yuji <53387749+pierrejaune@users.noreply.github.com>
2020-08-16 12:56:53 +0900

    implement delete feature

--- src/index.js
+++ src/index.js
@@ -4,21 +4,27 @@ import { createStore, applyMiddleware } from 'redux'; // createStoreはstoreを�
 import { Provider } from 'react-redux';// 作成したstoreを全コンポーネントに渡す為の機能をもつコンポーネント
 import thunk from 'redux-thunk';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import './index.css';
 import reducer from './reducers';
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk);
+const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
   <Provider store={ store }>
     <BrowserRouter>
       <Switch>
-        <Route exact  path="/events/new" component={EventsNew} />
-        <Route exact  path="/" component={EventsIndex} />
+        <Route path="/events/new" component={EventsNew} />
+        <Route path="/events/:id" component={EventsShow} />
+        <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
   </Provider>,
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,12 +1,25 @@
 import _ from 'lodash';
-import { READ_EVENTS } from '../actions'
+import {
+  CREATE_EVENT,
+  READ_EVENTS,
+  READ_EVENT,
+  UPDATE_EVENT,
+  DELETE_EVENT,
+} from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
+    case CREATE_EVENT:
+    case READ_EVENT:
+    case UPDATE_EVENT:
+        const data = action.response.data;
+        return { ...events, [data.id]: data }
     case READ_EVENTS:
-      // console.log(action.response.data);
       // console.log(_.mapKeys(action.response.data, 'id'));
       return _.mapKeys(action.response.data, 'id');
+    case DELETE_EVENT:
+      delete events[action.id]
+      return { ...events };
     default:
       return events;
   }
